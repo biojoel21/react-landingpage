472dcc8f85ef54839314aed9932c8b8023c6b610
biojoel21 <eebc_5@hotmail.com>
2025-02-20 11:58:22 -0400

    terminar workflow section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import FeaturesSection from './components/FeaturesSection'
+import Workflow from './components/Workflow'
+import Pricing from './components/Pricing'
 
 const App = () => {
   return (
      <>
        <Navbar />
        <div className="max-w-7xl mx-auto pt-20 px-6">
-        <HeroSection />
-        <FeaturesSection />
+          <HeroSection />
+          <FeaturesSection />
+          <Workflow />
+          <Pricing/>
        </div>       
      </>
   )
